bc13dceeb2a19f566291a26fd76d7910cf45ebb1
карина <@DESKTOP-FP9I0KA>
2024-09-26 14:41:30 +0300

    Add project files.

---
+++ Series/Program.cs
@@ -0,0 +1,30 @@
+﻿namespace Series
+{
+    internal class Program
+    {
+        static void Main()
+        {
+       
+            Console.WriteLine("Введіть рядок цифр:");
+            string input = Console.ReadLine();
+
+           
+            Console.WriteLine("Введіть довжину підрядка n:");
+            int n = int.Parse(Console.ReadLine());
+
+            if (n > input.Length)
+            {
+                Console.WriteLine("Підрядки довжиною більше, ніж рядок, неможливо отримати.");
+            }
+            else
+            {
+                
+                for (int i = 0; i <= input.Length - n; i++)
+                {
+                    string substring = input.Substring(i, n);
+                    Console.WriteLine(substring);
+                }
+            }
+        }
+    }
+}
